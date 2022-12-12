b5801336531ab834ee73ea41d7de0267d346d849
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-12-12 16:53:47 -0500

    Employer controller code added

--- WebProjectFinals/WebProjectFinals/controllers/EmployerController.cs
+++ WebProjectFinals/WebProjectFinals/controllers/EmployerController.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CareerConnect.Infrastructure.Models;
+using CareerConnect.Infrastructure.Service;
 using Microsoft.AspNetCore.Mvc;
+using WebProjectFinals.controllers;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace WebProjectFinals.controllers
+namespace CareerConnect.Controllers
 {
-    public class EmployerController : Controller
+    public class EmployerController : BaseController
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly UserService _userService;
+
+        public EmployerController(UserService userService)
         {
-            return View();
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index(EmployersVm vm)
+        {
+            var result = await _userService.GetEmployersAsync();
+
+            if (!result.Success || result.Data == null)
+            {
+                return BadRequest();
+            }
+            vm.Employers = result.Data;
+            return View(vm);
         }
     }
 }
-
